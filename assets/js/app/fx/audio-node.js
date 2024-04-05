cd987d9d419057d93a393e651c7a452c40cac581
Michael Chadwick <mike@neb.host>
2024-04-05 09:36:45 -0700

    added potential AudioWorkletNode class

---
+++ assets/js/app/fx/audio-node.js
@@ -0,0 +1,13 @@
+class RafflerAudioWorklet extends AudioWorkletProcessor {
+  process (inputs, outputs, parameters) {
+    const output = outputs[0]
+    output.forEach(channel => {
+      for (let i = 0; i < channel.length; i++) {
+        channel[i] = Math.random() * 1.0 - 0.5
+      }
+    })
+    return true
+  }
+}
+
+registerProcessor('raffler-audio-worklet', RafflerAudioWorklet)
